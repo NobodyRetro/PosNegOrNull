a43ad2b67f0e704701fbe256cc26e907bc8efb22
NobodyRetro <59867447+NobodyRetro@users.noreply.github.com>
2023-06-12 02:53:17 -0700

    Add files via upload

---
+++ PosNegOrNull/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Type in a number and I will tell you if it is Positive, Negative, or 0.");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number > 0)
+{
+    Console.WriteLine("The chosen number is Positive.");
+}
+else if (Number < 0)
+{
+    Console.WriteLine("The chosen number is Negative");
+}
+else 
+{
+    Console.WriteLine("The chosen number is Zero");
+}
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
